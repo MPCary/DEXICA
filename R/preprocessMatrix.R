1f133a712d7204e26c1ac01003ce501a49312aa9
Michael Cary <michael.cary.dev@gmail.com>
2017-06-24 16:28:43 -0700

    Simplified preprocessMatrix

--- R/preprocessMatrix.R
+++ R/preprocessMatrix.R
@@ -3,7 +3,7 @@
 #' @description Centers and/or scales the columns and/or rows of the input matrix.
 #'
 #' @usage preprocessMatrix(x, center.cols = TRUE, scale.cols = FALSE, center.rows = FALSE,
-#' scale.rows = FALSE, simultaneous = FALSE, verbose = TRUE)
+#' scale.rows = FALSE, verbose = TRUE)
 #'
 #' @param x a numeric matrix(-like object), typically with genes in rows and samples
 #' (e.g., microarrays) in columns
@@ -11,7 +11,6 @@
 #' @param scale.cols a logical value indicating whether to adjust columns to have unit variance
 #' @param center.rows a logical value indicating whether to adjust rows to have 0 mean
 #' @param scale.rows a logical value indicating whether to adjust rows to have unit variance
-#' @param simlutanous a logical value indicating whether to adjust rows and columns simultaneously
 #' @param verbose a logical value indicating the desired level of output as the algorithm runs
 #'
 #' @details This function centers and/or scales the columns and/or rows of the input matrix.
@@ -19,8 +18,7 @@
 #'
 #' @return A centered and/or scaled matrix.
 #'
-#' @seealso \code{\link[base]{scale}},
-#' \code{\link[scaleMatrix]{scaleMatrix}}
+#' @seealso \code{\link[base]{scale}}
 #'
 #' @examples
 #'x = matrix(rnorm(100), 10, 10)
@@ -29,23 +27,11 @@
 #' @export
 
 preprocessMatrix <- function(x, center.cols = TRUE, scale.cols = FALSE, center.rows = FALSE,
-                             scale.rows = FALSE, simultaneous = FALSE, verbose = FALSE) {
+                             scale.rows = FALSE, verbose = FALSE) {
   # Center and/or scale
-  if(!simultaneous) {
-    if(verbose) message("Centering and scaling...")
-    x = scale(x, center = center.cols, scale = scale.cols)
-    x = t(scale(t(x), center = center.rows, scale = scale.rows))
-  } else {
-    if(!("scaleMatrix" %in% rownames(installed.packages()))) {
-      stop("scaleMatrix package required for simultaneous scaling.")
-    } else {
-      require(scaleMatrix)
-      x = scaleMatrix(x, center.cols = center.cols, scale.cols = scale.cols,
-                      center.rows = center.rows, scale.rows = scale.rows,
-                      verbose = verbose)
-    }
-    stop("Sorry, simultaneous row/column scaling not yet implemented.")
-  }
+  if(verbose) message("Centering and scaling...")
+  x = scale(x, center = center.cols, scale = scale.cols)
+  x = t(scale(t(x), center = center.rows, scale = scale.rows))
 
   return(x)
 }
